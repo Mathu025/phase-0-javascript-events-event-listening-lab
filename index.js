61d5077f67a80ae8257cf7d2b623aea6050b6077
Mathu025 <mathualex72@gmail.com>
2025-06-22 10:52:13 +0300

    Complete Lab

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const newParagraph = document.createElement("p")
+    newParagraph.id  = "text"
+    newParagraph.textContent = "This is really cool!"
+    const main = document.getElementById("main")
+    main.appendChild(newParagraph)
+});
+
 function addingEventListener() {
     const button = document.getElementById('button')
     button.addEventListener('click', function() {
-        Alert ('I was Clicked')
+        alert ('I was Clicked')
     })
     
-}
+};
+
